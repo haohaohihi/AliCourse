7952c6017a5b732b887bbbae33ce828123c5e8a7
Liu Zhihao <zhihao.liu.work@gmail.com>
2017-11-15 10:30:42 +0800

    使用凡是获取成员方法

--- src/cc/ipotato/reflect/GetMethodTest.java
+++ src/cc/ipotato/reflect/GetMethodTest.java
@@ -1,4 +1,24 @@
 package cc.ipotato.reflect;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class GetMethodTest {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException {
+        Class<?> cls = Class.forName("cc.ipotato.reflect.Person");
+//        Method[] met = cls.getMethods();
+//        for(Method m: met){
+//            System.out.println(m);
+//        }
+        String attribute = "name";
+        String value = "haohao";
+        Object obj = cls.newInstance();
+
+        Method setMethod = cls.getMethod("setName", String.class);
+        setMethod.invoke(obj, value);
+
+        Method getMethod = cls.getMethod("getName");
+        Object result = getMethod.invoke(obj);
+        System.out.println(result);
+    }
 }
