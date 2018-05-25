7aeebc7e988cc49f7c39e295646b60dd9e4818ea
Liu Zhihao <haohaohihi@gmail.com>
2018-05-25 14:11:06 +0800

    使用PrepareeStatement，能够防止SQL注入

--- src/cc/ipotato/jdbc/PreparedStatementTest.java
+++ src/cc/ipotato/jdbc/PreparedStatementTest.java
@@ -10,18 +10,18 @@ import java.sql.SQLException;
  */
 public class PreparedStatementTest {
     public static String DBDRIVER = "com.mysql.jdbc.Driver";
-    public static String DBURL = "jdbc:mysql://10.108.157.14:3306/test";
+    public static String DBURL = "jdbc:mysql://10.108.157.158:3306/mybase";
     public static String DBUSER = "test";
-    public static String DBPASS = "hello_1234";
+    public static String DBPASS = "hello";
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName(DBDRIVER);
         Connection conn = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
-        String update_sql = "insert into stu(id, name) values(?,?)";
-        int id = 3;
-        String name = "Mr' Smith";
+        String update_sql = "insert into user(uname, upass) values(?,?)";
+        String name = "Smith";
+        String pass = "123456";
         PreparedStatement ps = conn.prepareStatement(update_sql);
-        ps.setInt(1, id);
-        ps.setString(2, name);
+        ps.setString(1, name);
+        ps.setString(2, pass);
         System.out.println(ps.executeUpdate());
     }
 }
