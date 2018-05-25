28d9d99e8d9e9c316bd421bfca6fe5f8c612f653
Liu Zhihao <haohaohihi@gmail.com>
2018-05-25 14:47:01 +0800

    将从JDBC读取到的数据，存入到对象中

---
+++ src/cc/ipotato/jdbc/Sort.java
@@ -0,0 +1,62 @@
+package cc.ipotato.jdbc;
+
+/**
+ * Created by haohao on 2018/5/25.
+ */
+public class Sort {
+    private int sid;
+    private String sname;
+    private double sprice;
+    private String sdesc;
+
+    public Sort(int sid, String sname, double sprice, String sdesc) {
+        this.sid = sid;
+        this.sname = sname;
+        this.sprice = sprice;
+        this.sdesc = sdesc;
+    }
+
+    public Sort() {}
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public void setSprice(double sprice) {
+        this.sprice = sprice;
+    }
+
+    public void setSdesc(String sdesc) {
+        this.sdesc = sdesc;
+    }
+
+    public int getSid() {
+        return sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public double getSprice() {
+        return sprice;
+    }
+
+    public String getSdesc() {
+        return sdesc;
+    }
+
+    @Override
+    public String toString() {
+        return "Sort{" +
+                "sid=" + sid +
+                ", sname='" + sname + '\'' +
+                ", sprice=" + sprice +
+                ", sdesc='" + sdesc + '\'' +
+                '}';
+    }
+}
---
+++ src/cc/ipotato/jdbc/SortReader.java
@@ -0,0 +1,24 @@
+package cc.ipotato.jdbc;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by haohao on 2018/5/25.
+ */
+public class SortReader {
+    public static void main(String[] args) throws SQLException {
+        Connection conn = JDBCUtils.getConnection();
+        String sql = "select * from sort";
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        List<Sort> list = new ArrayList<>();
+        while (rs.next()) {
+            list.add(new Sort(rs.getInt("sid"), rs.getString("sname"),
+                    rs.getDouble("sprice"), rs.getString("sdesc")));
+           }
+        JDBCUtils.close(conn, stmt, rs);
+        System.out.println(list);
+    }
+}
